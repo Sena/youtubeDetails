9c15fbc89ab79e35bf26453ee03e6c95230d1201
F. Sena <github@senartes.com.br>
2013-06-18 13:42:23 -0300

    Remove unnecessary code

--- class/youtubeDetails.php
+++ class/youtubeDetails.php
@@ -8,29 +8,24 @@ class youtubeDetails {
 		'small' => NULL);
 
 	public function __construct($url = null) {
-		if($url !== NULL) {
-			$this->getKey($url);
-		}
+		$this->getKey($url);
 	}
 
-	public function getImages($url = NULL, $size = 'small'){
-		if($url !== NULL) {
-			$this->getKey($url);
-		}
+	public function getImages($url = NULL){
+		$this->getKey($url);
 		$this->image['big'] = 'http://img.youtube.com/vi/' . $this->key . '/0.jpg';
 		$this->image['small'] = 'http://i1.ytimg.com/vi/' . $this->key . '/default.jpg';
 	    return $this->image;
 	}
 	public function getEmbed($url = NULL){
-		if($url !== NULL) {
-			$this->getKey($url);
-		}
+		$this->getKey($url);
 		return $this->embed = 'http://www.youtube.com/embed/' . $this->key;
 	}
 	public function getInfor($url = NULL){
-		$this->gData($url);
-		$this->getImages($url);
-		$this->getEmbed($url);
+		$this->getKey($url);
+		$this->gData();
+		$this->getImages();
+		$this->getEmbed();
 
 		return array('image' => $this->image,
 			'key' => $this->key,
@@ -41,15 +36,16 @@ class youtubeDetails {
 			'author' => array('name' => (string)$this->data->author->name,
 				'uri' => (string)$this->data->author->uri));
 	}
-	private function getKey($url){
-	    $url = explode('v=',$url);
-	    $url = explode('&',$url[1]);
-	    return $this->key = current($url);
+	private function getKey($url = NULL){
+		if($url !== NULL) {
+		    $url = explode('v=',$url);
+		    $url = explode('&',$url[1]);
+		    $this->key = current($url);
+		}
+	    return $this->key;
 	}
 	private function gData($url = NULL) {
-		if($url !== NULL) {
-			$this->getKey($url);
-		}
+		$this->getKey($url);
 		$curl = curl_init('http://gdata.youtube.com/feeds/api/videos/' . $this->key);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		$xml = curl_exec($curl);
